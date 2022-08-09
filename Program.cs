5cf85c841a6ac371ea27a63a5194d7591fc4a479
staocube <staocube@gmail.com>
2022-08-09 14:18:05 +0100

    Chabox SignalR commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using SignalRWebpack.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.MapHub<ChatHub>("/hub");
+
+app.Run();
